88da8d4ae647c72db4de788b5750335944129590
Aayush Kumar Sahu <aayushmau5@gmail.com>
2023-11-20 21:22:29 +0530

    consider # in links

--- content.js
+++ content.js
@@ -1,20 +1,20 @@
-// content.js
 function addBlankTargetToLinks() {
   browser.runtime.sendMessage({ action: "checkPinned" }, (response) => {
-    console.log(response);
     if (response && response.isPinned) {
-      const links = document.querySelectorAll('a');
-      console.log(links);
-      links.forEach(link => {
-        // Check if the 'target' attribute is not already present
-        if (!link.hasAttribute('target')) {
-          link.setAttribute('target', '_blank');
+      const links = document.querySelectorAll("a");
+
+      links.forEach((link) => {
+        // ignore links that start with "#"
+        if (!link.getAttribute("href").startsWith("#")) {
+          // Check if the 'target' attribute is not already present
+          if (!link.hasAttribute("target")) {
+            link.setAttribute("target", "_blank");
+          }
         }
       });
     }
   });
 }
 
-// Run the function when the DOM is ready
 // document.addEventListener('DOMContentLoaded', addBlankTargetToLinks);
 addBlankTargetToLinks();
